09ec911263ea2aea297185f826ac1384e0e50975
anjalig <anjali.gosain91@gmail.com>
2020-09-07 10:35:49 -0700

    revert changes

--- admision-service-ms/admision-service-ms/src/main/java/com/agc/admisionservicems/AdmisionServiceMsApplication.java
+++ admision-service-ms/admision-service-ms/src/main/java/com/agc/admisionservicems/AdmisionServiceMsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication(scanBasePackages = {"com.agc"})
-@EnableConfigServer
 public class AdmisionServiceMsApplication {
 
 	public static void main(String[] args) {
